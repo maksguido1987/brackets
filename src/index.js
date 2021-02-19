98891a6e4f534ecf1e947afd82030c27db4f608a
Your Name <you@example.com>
2021-02-19 13:26:07 +0300

    wwww

--- src/index.js
+++ src/index.js
@@ -21,19 +21,33 @@ module.exports = function check(str, bracketsConfig) {
   // }
   // return stack.length === 0;
 
-  const open = new Set(bracketsConfig.map(pair => pair[0]));
-  const close  = new Set(bracketsConfig.map(pair => pair[1]));
-  const relevant = bracketsConfig.reduce((acc, [open, close]) => ({...acc, [close]: open}), {});
+  // const open = new Set(bracketsConfig.map(pair => pair[0]));
+  // const close  = new Set(bracketsConfig.map(pair => pair[1]));
+  // const relevant = bracketsConfig.reduce((acc, [open, close]) => ({...acc, [close]: open}), {});
  
-  let stack = [];
-  for (let char of str) {
-    if (open.has(char)) {
-      stack.push(char);
-    }
-    if (close.has(char)) {
-      if (relevant[char] !== stack.pop()) return false;
-    }
-  }
+  // let stack = [];
+  // for (let char of str) {
+  //   if (open.has(char)) {
+  //     stack.push(char);
+  //   }
+  //   if (close.has(char)) {
+  //     if (relevant[char] !== stack.pop()) return false;
+  //   }
+  // }
   
-  return stack.length === 0;
+  // return stack.length === 0;
+  if (str.length % 2 !== 0) return false;
+
+  while ( str.includes('()') ||
+          str.includes('[]') ||
+          str.includes('{}') ||
+          str.includes('||') ||
+          str.includes('12') ||
+          str.includes('34') ||
+          str.includes('56') ||
+          str.includes('77') ||
+          str.includes('88')) {
+        str = str.replace('()', '').replace('[]', '').replace('{}', '').replace('||', '').replace('12', '').replace('34', '').replace('56', '').replace('77', '').replace('88', '')   
+  }
+  return str === ''
 }
